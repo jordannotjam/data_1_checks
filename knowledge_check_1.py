c972046ff197bef6bed75ee2f05cf970e841e78f
Jordan Meyer <jordannotjam@gmail.com>
2022-09-26 14:35:48 -0400

    completed the 4 challenges

---
+++ knowledge_check_1.py
@@ -0,0 +1,13 @@
+print ('hello world!')
+
+
+my_list = ['Scooby', 'Flinstones', 'Jetsons']
+print(my_list[2])
+
+
+knowledge_dict = {1: 'Digimon', 2: 'Pokemon'}
+print(knowledge_dict[1])
+
+
+tuple1 = ('comedy', 'horror', 'drama', 'romance')
+print(tuple1[1])
